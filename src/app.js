61b538352bbc0987fe6cd5cf68ecb9e3e0fb9bd5
Steve Mask <steve@scaledinference.com>
2018-05-03 11:08:09 -0700

    cart init

--- src/app.js
+++ src/app.js
@@ -1,49 +1,98 @@
-const productContext = {
+const products = {
   categories: categories
 };
 
-const cartContext = {
-
-};
+const cart = [];
 
 window.addEventListener('DOMContentLoaded', (e) => {
-  document.querySelector('.main').innerHTML = Handlebars.templates.product(productContext);
+  addEventsForProductPage();
 
-  document.querySelectorAll('.product').forEach(element => { 
-    element.addEventListener('mouseover', (e) => {
-      element.querySelector('.overlay').className = 'overlay show';
-    });
-    element.addEventListener('mouseout', (e) => {
-      element.querySelector('.overlay').className = 'overlay hide';
-    });
+  document.querySelector('.ctaBtn').addEventListener('click', (e) => {
+    const label = e.target.textContent;
+
+    switch (label) {
+      case 'Checkout':
+      location.hash = '/checkout';
+      break;
+
+      case 'Submit Order':
+      location.hash = '/thank_you';
+      break;
+
+      case 'Continue Shopping':
+      location.hash = '';
+      break;
+
+      default:
+      location.hash = '/cart';
+    }
   });
 });
 
-window.addEventListener('popstate', (e) => {
+window.addEventListener('hashchange', (e) => {
+  console.log('hashchange');
+  handleNavigation();
+});
+
+function handleNavigation() {
   let html;
 
   switch (location.hash) {
-    case '#/':
-    html = Handlebars.templates.product(productContext);
-    break;
-
-    case '#/product':
-    html = Handlebars.templates.product(productContext);
+    case '/', '':
+    html = addEventsForProductPage();
+    document.querySelector('.ctaBtn').textContent = `Cart - ${cart.length} Items`;
     break;
 
     case '#/cart':
-    html = Handlebars.templates.cart(cartContext);
+    html = Handlebars.templates.cart(cart);
+    document.querySelector('.ctaBtn').textContent = 'Checkout';
     break;
-
+  
     case '#/checkout':
     html = Handlebars.templates.checkout(checkoutContext);
-
+    document.querySelector('.ctaBtn').textContent = 'Submit Order';
+  
     case '#/thank_you':
     html = Handlebars.templates.thankYou(thankYouContext);
-
+    document.querySelector('.ctaBtn').textContent = 'Continue Shopping';
+  
     default:
-    html = Handlebars.templates.product(productContext);
+    html = Handlebars.templates.product(products);
   }
-
+  
   document.querySelector('.main').innerHTML = html;
-});
+}
+
+function addEventsForProductPage() {
+  const html = Handlebars.templates.product(products);
+  document.querySelector('.main').innerHTML = html;
+
+  setTimeout(() => {
+    document.querySelectorAll('.product').forEach(element => { 
+      console.log('add product events');
+      element.addEventListener('mouseover', (e) => {
+        element.querySelector('.overlay').className = 'overlay show';
+      });
+
+      element.addEventListener('mouseout', (e) => {
+        element.querySelector('.overlay').className = 'overlay hide';
+      });
+    });
+
+    document.querySelectorAll('.addToCart').forEach(element => {
+      element.addEventListener('click', (e) => {
+        const target = e.target;
+        cart.push({
+          id: target.dataset.id,
+          name: target.dataset.name,
+          description: target.dataset.description,
+          price: target.dataset.price
+        });
+
+        document.querySelector('.ctaBtn').textContent = `Cart - ${cart.length} Items`;
+      });
+    });
+  }, 500);
+
+  return html;
+}
--- src/templates.js
+++ src/templates.js
@@ -1,7 +1,23 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['cart'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div>\n  Cart\n</div>";
+templates['cart'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "    <div class=\"item\">\n      <div class=\"thumbnail\">\n        <img src=\"images/products/"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + ".jpg\" alt=\"image\">\n      </div>\n      <div class=\"desc\">\n        <div class=\"name\">"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</div>\n        <div class=\"description\">"
+    + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
+    + "</div>\n      </div>\n      <div class=\"price\">"
+    + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
+    + "</div>\n    </div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div>\n  <h1>Shopping Cart</h1>\n  <div class=\"cart\">\n"
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.cart : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    <hr/>\n    <div class=\"summary\">\n      <div class=\"subtotal\"></div>\n      <div class=\"discount\"></div>\n      <hr/>\n      <div class=\"total\"></div>\n      <div class=\"checkoutBtn\"></div>\n    </div>\n  </div>\n</div>";
 },"useData":true});
 templates['checkout'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     return "";
@@ -25,7 +41,15 @@ templates['product'] = template({"1":function(container,depth0,helpers,partials,
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
     + "</div>\n          <div class=\"price\">"
     + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
-    + "</div>\n          <div class=\"overlay hide\">\n            <div class=\"addToCart\">Add To Cart</div>\n          </div>\n        </div>\n";
+    + "</div>\n          <div class=\"overlay hide\">\n            <div class=\"addToCart\" data-id="
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + " data-name="
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + " data-description="
+    + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
+    + " data-price="
+    + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
+    + ">Add To Cart</div>\n          </div>\n        </div>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
