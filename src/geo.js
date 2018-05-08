194b48427d8446c97e52cf649489132bd089e827
Steve Mask <steve@scaledinference.com>
2018-05-08 09:36:12 -0700

    add mobile demo

---
+++ src/geo.js
@@ -0,0 +1,18 @@
+function geoLocation() {
+  const countries = ['United States', 'Great Britain', 'Russia', 'Ukraine', 'Israel', 'India', 'China', 'Turkey'];
+  const regions = {
+    'United States': ['Arizona', 'California', 'Idaho', 'Oregon', 'Utah', 'Washington'],
+    'Great Britain': ['England', 'Northern Ireland', 'Scotland', 'Wales'],
+    'Russia': ['Adygea', 'Altai Republic', 'Bashkortostan', 'Buryatia', 'Dagestan'],
+    'Ukraine': ['Autonomous Republic of Crimea','Cherkasy Region', 'Kharkiv Region', 'Kyiv Region', 'Sumy Region'],
+    'Israel': ['Jerusalem District', 'Northern District', 'Haifa District', 'Tel Aviv District', 'Judea and Samaria'],
+    'India': ['Ajmer-Merwara', 'Assam', 'Baluchistan', 'Bengal', 'Bihar', 'Bombay'],
+    'China': ['Beijing Municipality', 'Tianjin Municipality', 'Jilin Province', 'Shanghai Municipality', 'Jiangsu Province'],
+    'Turkey': ['Adana', 'Ankara', 'Balıkesir', 'Bursa', 'Edirne', 'Istanbul', 'Ordu', 'Batman']
+  };
+
+  const country = countries[Math.round(Math.random(countries.length - 1))];
+  const region = regions[country][Math.round(Math.random(regions.length - 1))];
+
+  return { country, region};
+}
