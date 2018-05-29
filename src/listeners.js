353241dab257040783b883cc5f2d8d80f53c5d80
Steve Mask <steve@scaledinference.com>
2018-05-29 09:00:00 -0700

    remove random context

--- src/listeners.js
+++ src/listeners.js
@@ -1,42 +1,42 @@
 import loadScript from './loadScript.js';
 import { run } from './demo.js';
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
    
-  if (localStorage.getItem("key") == "") {
-    document.querySelector(".run").dataset.count = 0;
-    localStorage.setItem("count", 0);
+  if (localStorage.getItem('key') == '') {
+    document.querySelector('.run').dataset.count = 0;
+    localStorage.setItem('count', 0);
   } else {
     if (localStorage.getItem('count') === null) {
       localStorage.setItem('count', 0);
     } else {
       localStorage.setItem('count', localStorage.getItem('count'));
     }
-    document.querySelector(".run").dataset.count = localStorage.getItem("count");
+    document.querySelector('.run').dataset.count = localStorage.getItem('count');
     
     loadScript();
   }
 });
 
-document.querySelector("#key").addEventListener("keyup", function(e) {
-  if (typeof Amp === "undefined") { // invalid key or not loaded
-    localStorage.setItem("key", e.target.value);
+document.querySelector('#key').addEventListener('keyup', function(e) {
+  if (typeof Amp === 'undefined') { // invalid key or not loaded
+    localStorage.setItem('key', e.target.value);
     loadScript();
   } else {
     var newKey = e.target.value;
-    var oldKey = localStorage.getItem("key");
+    var oldKey = localStorage.getItem('key');
 
-    localStorage.setItem("key", e.target.value);
+    localStorage.setItem('key', e.target.value);
     
     if (newKey.indexOf(oldKey) === -1) {
-      localStorage.setItem("count", 0);
+      localStorage.setItem('count', 0);
       loadScript(); 
     } 
   }
 });
 
-document.querySelector(".run").addEventListener("click", function(e) {
-  if (typeof Amp !== "undefined") {
+document.querySelector('.run').addEventListener('click', function(e) {
+  if (typeof Amp !== 'undefined') {
     if (localStorage.getItem('count') === null) {
       localStorage.setItem('count', 0);
     }
@@ -45,33 +45,34 @@ document.querySelector(".run").addEventListener("click", function(e) {
     e.target.dataset.count = localStorage.getItem('count');
 
     amp.session = new amp.Session();
-    amp.config.set("syncInterval", 1000);
+    amp.config.set('syncInterval', 1000);
         
     run();
   }
 });
 
-document.querySelector(".runMany").addEventListener("click", function(e) {
-  if (typeof Amp !== "undefined") {
+document.querySelector('.runMany').addEventListener('click', function(e) {
+  if (typeof Amp !== 'undefined') {
     if (localStorage.getItem('count') === null) {
       localStorage.setItem('count', 0);
     }
 
     var button = e.target;
-    if (button.innerHTML.indexOf("Run Many") != -1) {
-      button.style.backgroundColor = "rgb(252,76,71)";
-      button.innerHTML = "Pause";
+    if (button.innerHTML.indexOf('Run Many') != -1) {
+      button.style.backgroundColor = 'rgb(252,76,71)';
+      button.innerHTML = 'Pause';
 
       window.sync = setInterval(function() {
-        amp.config.set("syncInterval", 1000);
+        amp.config.set('syncInterval', 1000);
       }, 1000);
 
       window.interval = setInterval(function(){
         localStorage.setItem('count', (parseInt(localStorage.getItem('count'), 10) + parseInt(1, 10)));
-        document.querySelector(".run").dataset.count = localStorage.getItem('count');
+        document.querySelector('.run').dataset.count = localStorage.getItem('count');
         
-        const contextValue = ['connection', 'location', 'return', 'timeOfDay'][Math.floor(Math.random() * 4)];
-        document.querySelector('#stories').value = contextValue;
+        // uncomment for random context selection
+        // const contextValue = ['connection', 'location', 'return', 'timeOfDay'][Math.floor(Math.random() * 4)];
+        // document.querySelector('#stories').value = contextValue;
 
         amp.session = amp.Session();
         
@@ -79,16 +80,20 @@ document.querySelector(".runMany").addEventListener("click", function(e) {
       }, 20);
 
       setTimeout(function() {
-        button.style.backgroundColor = "orange";
-        button.innerHTML = "Run Many";
+        button.style.backgroundColor = 'orange';
+        button.innerHTML = 'Run Many';
         clearInterval(window.interval);
         clearInterval(window.sync);
       }, 600000);
     } else {
-      button.style.backgroundColor = "orange";
-      button.innerHTML = "Run Many";
+      button.style.backgroundColor = 'orange';
+      button.innerHTML = 'Run Many';
       clearInterval(window.interval);
       clearInterval(window.sync);
     }
   }
+});
+
+document.querySelector('#stories').addEventListener('change', function(e) {
+  document.querySelector('#key').value = '';
 });
