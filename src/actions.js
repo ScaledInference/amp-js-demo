413a25ea8151b7b4a1cc31b33a1f6d4dc5f08efb
Steve Mask <steve@scaledinference.com>
2018-05-30 07:50:44 -0700

    update action labels

--- src/actions.js
+++ src/actions.js
@@ -22,22 +22,30 @@ export default function takeAction() {
   const addButton = document.querySelector('.variant2 .result .icon .add');
   const buyButton = document.querySelector('.variant2 .result .icon .buy');
   const quickButton = document.querySelector('.variant2 .result .icon .quick');
+  const label = document.querySelector('.variant2 .result .label');
 
   addButton.style.visibility = 'visible';
+  label.textContent = '3-Step Checkout';
 
   if (decision.btnSequence === 'addBuy') {
     buyButton.style.visibility = 'visible';
+    label.textContent = '2/3-Step Checkout';
   } else if (decision.btnSequence === 'addQuick') {
     quickButton.style.visibility = 'visible';
+    label.textContent = '1/3-Step Checkout';
   } else if (decision.btnSequence === 'addBuyQuick') {
+    label.textContent = '1/2/3-Step Checkout';
     buyButton.style.visibility = 'visible';
     quickButton.style.visibility = 'visible';
   }
 
+  const rushLabels = document.querySelectorAll('.variant3 .result .unit');
   // checkout action
   if (decision.rushShipping === 0) {
     checkoutIcon.textContent = '🚫';
+    rushLabels.forEach(label => label.style.visibility = 'hidden');
   } else {
     checkoutIcon.textContent = decision.rushShipping;
+    rushLabels.forEach(label => label.style.visibility = 'visible');
   }
 }
